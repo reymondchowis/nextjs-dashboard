2f16ddf59b6ccb219d239656259feea5769e07ef
Reymond <140043415+reymondchowis@users.noreply.github.com>
2023-11-23 10:29:12 +0800

    Add files via upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Indie_Flower, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets : ['greek']})
+export const customInter = Inter({ subsets : ['greek']})
+export const lusitana = Lusitana({ weight: ['400' , '700'], subsets : ['latin']})
